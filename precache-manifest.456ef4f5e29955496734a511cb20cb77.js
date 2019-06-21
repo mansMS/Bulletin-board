a1e5233b5d51ab596ab015605527b30a09271275
MS Mansurov <snam1995@mail.ru>
2019-06-21 03:49:57 +0300

    Updates

---
+++ precache-manifest.456ef4f5e29955496734a511cb20cb77.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "317afd29cb835426e047f02056e9fc45",
+    "url": "/Bulletin-board/index.html"
+  },
+  {
+    "revision": "e48ddf7f5c1d8ca115b3",
+    "url": "/Bulletin-board/static/css/2.482555ae.chunk.css"
+  },
+  {
+    "revision": "bbe9504a2bb347658a36",
+    "url": "/Bulletin-board/static/css/main.1a80ed9a.chunk.css"
+  },
+  {
+    "revision": "e48ddf7f5c1d8ca115b3",
+    "url": "/Bulletin-board/static/js/2.e9ace8ab.chunk.js"
+  },
+  {
+    "revision": "bbe9504a2bb347658a36",
+    "url": "/Bulletin-board/static/js/main.84232aaa.chunk.js"
+  },
+  {
+    "revision": "707fce2ee84dbc60a3ae",
+    "url": "/Bulletin-board/static/js/runtime~main.6cb1d615.js"
+  },
+  {
+    "revision": "bab76559fd71ea8918d868e4ce35dbc2",
+    "url": "/Bulletin-board/static/media/checkIcon.bab76559.svg"
+  },
+  {
+    "revision": "2824febb14f706217585b299f7045350",
+    "url": "/Bulletin-board/static/media/exclamationIcon.2824febb.svg"
+  },
+  {
+    "revision": "7b2da63f7269402d5e2a49975c74330e",
+    "url": "/Bulletin-board/static/media/phoneIcon.7b2da63f.svg"
+  },
+  {
+    "revision": "c5e54c1d7472dc0bbf1f274c9ed0c5c9",
+    "url": "/Bulletin-board/static/media/questionIcon.c5e54c1d.svg"
+  }
+]);
